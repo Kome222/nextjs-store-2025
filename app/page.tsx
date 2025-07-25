6efb60a617f527f87ab70d9e146d0936de1fb3a9
Kome222 <185741291+Kome222@users.noreply.github.com>
2025-07-25 04:21:55 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,4 @@
-import { Carousel } from 'components/carousel';
-import { ThreeItemGrid } from 'components/grid/three-items';
-import Footer from 'components/layout/footer';
 
-export const metadata = {
-  description:
-    'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
-  openGraph: {
-    type: 'website'
-  }
-};
 export default function Home() {
   return (
     <main style={{ padding: "2rem", fontFamily: "sans-serif", textAlign: "center" }}>
